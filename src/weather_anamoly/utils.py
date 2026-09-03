0f222a57033c8471f7af813b9a12cdd4b59a128d
Kalyan_udd <kalyansaiuddagiri22@gmail.com>
2026-09-04 00:29:26 +0530

    Importdata object building with proper testing and adding cache in the data to extract faster

---
+++ src/weather_anamoly/utils.py
@@ -0,0 +1,40 @@
+from retry_requests import retry
+import requests_cache
+import openmeteo_requests
+import pandas as pd
+
+class ImportData:
+    def __init__(self, cache_path : str = "data/weather_cache"):
+        self.Cached_session = requests_cache.CachedSession(cache_path, expire_after=-1)
+        self.retry_session = retry(self.Cached_session, retries=5, backoff_factor=0.2)
+        self.openmeteo = openmeteo_requests.Client(session=self.retry_session)
+        self.dataframe = None
+        self.hourly = None
+
+    def extract(self, start_date: str, end_date: str, latitude: float, longitude: float):
+        url = "https://archive-api.open-meteo.com/v1/archive"
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "start_date": start_date,
+            "end_date": end_date,
+            "hourly": ['temperature_2m', 'relative_humidity_2m', 'surface_pressure'],
+            "timezone": "Asia/Kolkata"
+        }
+
+        response = self.openmeteo.weather_api(url=url, params=params)[0]
+        hourly = response.Hourly()
+        self.hourly = hourly
+        self.dataframe = pd.DataFrame(
+            {
+                "date": pd.date_range(
+                    start=pd.to_datetime(self.hourly.Time(), unit="s", utc=True),
+                    end=pd.to_datetime(self.hourly.TimeEnd(), unit="s", utc=True),
+                    freq=pd.Timedelta(seconds=self.hourly.Interval()),
+                    inclusive="left"
+                ),
+                "temperature": self.hourly.Variables(0).ValuesAsNumpy(),
+                "humidity": self.hourly.Variables(1).ValuesAsNumpy(),
+                "pressure": self.hourly.Variables(2).ValuesAsNumpy(),
+            }
+        ).set_index("date")
